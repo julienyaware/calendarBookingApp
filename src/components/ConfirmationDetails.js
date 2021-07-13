1152a34e85892c8266b5f8308603a7aa18a29926
Julienyaware <julienyaware@gmail.com>
2021-07-13 07:10:12 +0300

    Added styling for confirmation form

--- src/components/ConfirmationDetails.js
+++ src/components/ConfirmationDetails.js
@@ -1,63 +1,74 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import db from '../services/Db'
 import TimeSlots from './TimeSlots';
 
 function ConfirmationDetails() {
 
-    const [reason, setReason] = useState("")
-    const [name  , setName] = useState("");
-    const [time , setTime] = useState("");
-    const [showTimeSlots,setShowTimeSlots] = useState(false)
+  const [reason, setReason] = useState("")
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
+  const [showTimeSlots, setShowTimeSlots] = useState(false)
 
-    const sub = (e) => {
-      e.preventDefault();
-        
-      // Add data to the store
-      db.collection("data").add({
-          Name: name,
-          Time: time,
-          Reason: reason
-      })
+  const sub = (e) => {
+    e.preventDefault();
+
+    // Add data to the store
+    db.collection("data").add({
+      Name: name,
+      Time: time,
+      Reason: reason
+    })
       .then((docRef) => {
-          alert("Appointment with mentor confirmed")
+        alert("Appointment with mentor confirmed")
+        refreshPage()
       })
       .catch((error) => {
-          console.error("Error booking appointment: ", error)
-          alert("Error booking appointment: ", error)
+        console.error("Error booking appointment: ", error)
+        alert("Error booking appointment: ", error)
       });
   }
 
-  const triggerShowTimeSlots = ()=>{
+  const triggerShowTimeSlots = () => {
     setShowTimeSlots(true)
   }
-    return (
-        <div>
-          {!showTimeSlots && (
-            <center>
-             <button className="timeStop" onClick={triggerShowTimeSlots}>Select another time slot</button>
-                <form 
-                  onSubmit={(event) => {sub(event)}}>
-                    <input type="text" placeholder="your name"
-                      onChange={(e)=>{setName(e.target.value)}} />
-                      <br/><br/>
-                    <textarea type="text" placeholder="your reason"
-                      onChange={(e)=>{setReason(e.target.value)}}></textarea>
-                      <br/><br/>
-                    <input type="text" placeholder="Time"
-                      onChange={(e)=>{setTime(e.target.value)}}/>
-                      <br/><br/>
-                    <button className="timeStop" type="submit">Submit</button>
-                </form>
-            </center>
-          )}
 
-          {showTimeSlots &&(
-            <TimeSlots/>
-          )}
-           
-        </div>
-        
-    )
+  const refreshPage = () => {
+    window.location.reload()
+  }
+  return (
+    <div>
+      {!showTimeSlots && (
+        <center className="confirmationForm">
+          <button className="timeStop" onClick={triggerShowTimeSlots}>Select another time slot</button>
+          <form
+            onSubmit={(event) => { sub(event) }}>
+            <label>User Name:</label>
+            <br />
+            <input type="text" placeholder="your name" required
+              onChange={(e) => { setName(e.target.value) }} />
+            <br /><br />
+            <label>Reason for booking an appointment:</label>
+            <br />
+            <textarea type="text" placeholder="your reason" required
+              onChange={(e) => { setReason(e.target.value) }}></textarea>
+            <br /><br />
+            <label>Selected time slot:</label>
+            <br />
+            <input type="text" placeholder="Time"
+              onChange={(e) => { setTime(e.target.value) }} />
+            <br /><br />
+            <button className="timeStop" type="submit">Submit</button>
+          </form>
+        </center>
+      )}
+
+      {showTimeSlots && (
+        <TimeSlots />
+      )}
+
+    </div>
+
+  )
 }
 
 export default ConfirmationDetails
